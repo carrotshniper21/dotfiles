0a2ac72e9214d395af122cc32e5f1dca647eca19
eatmynerds <vipershniper07@gmail.com>
2024-03-12 14:05:24 +0000

    Update installation script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 pre_install() {
     pacman -S git --needed base-devel --noconfirm || exit
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable -y
+    rustup default stable
 }
 
 post_install() {
