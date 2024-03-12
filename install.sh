dbe71618784cc6c2cb8fb7a97a07bfff4959da1f
eatmynerds <vipershniper07@gmail.com>
2024-03-12 07:31:49 +0000

    Update installation script

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ case "$1" in
       cd yay || exit
       makepkg -si --noconfirm || exit
       yay -S skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd nautilus fzf networkmanager exa bat gh zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig --noconfirm
-      git clone https://github.com/fdev31/picom || exit
       cd .. && rm -rf yay 
+      git clone https://github.com/fdev31/picom || exit
       cd picom || exit
       meson setup --buildtype=release . build || exit
       ninja -C build || exit
