f3b3c54497ceadb1227a84ba6842f909446329ce
eatmynerds <vipershniper07@gmail.com>
2024-03-12 10:08:43 +0000

    Update installation script

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ case "$1" in
       cd yay || exit
       makepkg -si --noconfirm || exit
       yay -S neofetch skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd nautilus fzf networkmanager exa bat github-cli zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig --noconfirm
-      cd .. && rm -rf yay 
+      cd .. && rm -rf yay
+      
       git clone https://github.com/fdev31/picom || exit
       cd picom || exit
       meson setup --buildtype=release . build || exit
@@ -18,35 +19,42 @@ case "$1" in
       cd .. && rm -rf picom
       ;;
   --dots)
-      git clone https://github.com/carrotshniper21/dotfiles
+      git clone https://github.com/carrotshniper21/dotfiles || exit
       cd dotfiles || exit
+      
       chsh $USER -s /bin/zsh
       rm $HOME/.bash*
-      echo "Copying backgrounds..."
+      
+      echo "(1/6) Configuring backgrounds"
       cp -r backgrounds $HOME/.backgrounds
-      echo "Copying dwmblocks binaries..."
-      sudo cp bin/* /usr/bin || exit 
-      echo "Copying configuration files..."
-      cp config/picom.conf $HOME/.config/picom.conf
-      cp config/xsession $HOME/.xsession
-      echo "Copying desktop configuration..."
+      
+      echo "(2/6) Copying configuration files (picom, mpv, cava, neofetch, ranger, wezterm, rofi, skippy-xd)"
+      cp config/picom.conf $HOME/.config
+      cp -r config/mpv $HOME/.config
+      cp -r config/cava $HOME/.config
+      cp -r config/neofetch $HOME/.config
+      cp -r config/ranger $HOME/.config
+      cp -r config/wezterm $HOME/.config
+      cp -r config/rofi $HOME/.config
+      cp -r config/skippy-xd $HOME/.config
+
+      echo "(3/6) Copying desktop configuration files"
+      cp desktop/xsession $HOME/.xsession
       sudo cp desktop/dwm.desktop /usr/share/xsessions || exit
-      cp -r desktop/skippy-xd $HOME/.config
-      echo "Building (dwm, dwmblocks)"
+      
+      echo "(4/6) Building (dwm, dwmblocks)"
       sudo make clean install -C desktop/dwm || exit
       sudo make clean install -C desktop/dwmblocks || exit
-      echo "Media configuration (mpv, cava)"
-      cp -r media/mpv $HOME/.config
-      cp -r media/music/cava $HOME/.config
-      echo "Configuring launchers (rofi)"
-      cp -r desktop/rofi $HOME/.config
-      echo "Configuring terminal"
-      cp -r terminal/neofetch $HOME/.config
-      cp -r terminal/ranger $HOME/.config
-      cp -r terminal/wezterm $HOME/.config
-      cp fzf.zsh $HOME/.fzf.zsh
+
+      echo "(5/6) Moving dwmblocks binaries to /usr/bin"
+      sudo cp bin/* /usr/bin || exit 
+      
+      echo "(6/6) Configuring terminal (fzf, zsh)"
       sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
       rm $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+      cp terminal/fzf.zsh $HOME/.fzf.zsh
       cp terminal/zshrc $HOME/.zshrc
+      echo "DONE!"
       ;;
 esac
+
