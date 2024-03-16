c1852eda2fba491485634fadc10cf907258e6b78
Jason Spalti <vipershniper07@gmail.com>
2024-03-16 17:47:20 -0500

    refactor: changed lunarvim installation

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ post_install() {
     cd yay || exit
     makepkg -si --noconfirm || exit
     cd .. && rm -rf yay || exit
-    yay -S neofetch skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd thunar lxappearance fzf networkmanager exa bat github-cli zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig nodejs npm python-pip libsixel imagemagick qt5‑graphicaleffects qt5‑quickcontrols2 qt5‑svg --noconfirm || exit
+    yay -S neofetch skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd thunar lxappearance fzf networkmanager exa bat github-cli zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig nodejs npm python-pip libsixel imagemagick  --noconfirm || exit
 
     if [ -e /etc/systemd/system/display-manager.service ]; then 
         default_target=$(basename $(readlink -f /etc/systemd/system/display-manager.service))
@@ -21,6 +21,8 @@ post_install() {
       yay -S sddm --noconfirm || exit
       sudo systemctl enable sddm.service || exit
     fi
+
+    yay -S qt5‑graphicaleffects qt5‑quickcontrols2 qt5‑svg || exit
 
     git clone https://github.com/fdev31/picom || exit
     cd picom || exit
@@ -42,6 +44,7 @@ configure_dots() {
 
     echo "(2/8) Installing lunarvim..."
     bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+    sudo cp .local/bin/lvim /usr/bin/lvim || exit
 
     echo "(3/8) Copying configuration files..."
     cp -r config/* $HOME/.config || exit
@@ -53,10 +56,10 @@ configure_dots() {
     cp desktop/xsession $HOME/.xsession
     sudo cp desktop/dwm.desktop /usr/share/xsessions || exit
     sudo cp -r desktop/tartarus -r /usr/share/grub/themes || exit
-    sudo mv desktop/grub /etc/default/grub || exit
+    sudo cp desktop/grub /etc/default/grub || exit
     sudo grub-mkconfig -o /boot/grub/grub.cfg || exit
     sudo cp -r desktop/sugar-candy /usr/share/sddm/themes || exit
-    sudo mv desktop/default.conf /usr/lib/sddm/sddm.conf.d/default.conf || exit
+    sudo cp desktop/default.conf /usr/lib/sddm/sddm.conf.d/default.conf || exit
 
     echo "(6/8) Building dwm and dwmblocks..."
     sudo make clean install -C desktop/dwm || exit
