2822d66b9dc73fe5df9550c4fa411fa4ee456e33
eatmynerds <vipershniper07@gmail.com>
2024-03-16 21:36:53 +0000

    feat: sddm & grub themes now

--- install.sh
+++ install.sh
@@ -8,8 +8,19 @@ post_install() {
     git clone https://aur.archlinux.org/yay.git || exit
     cd yay || exit
     makepkg -si --noconfirm || exit
-    yay -S neofetch skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd thunar lxappearance fzf networkmanager exa bat github-cli zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig nodejs npm python-pip libsixel imagemagick --noconfirm || exit
     cd .. && rm -rf yay || exit
+    yay -S neofetch skippy-xd cava ranger mpv rofi wezterm zsh feh xorg-xrandr neovim flameshot notify-osd discord chromium xclip pavucontrol ttf-roboto-mono-nerd thunar lxappearance fzf networkmanager exa bat github-cli zsh-autosuggestions zsh-syntax-highlighting meson ninja uthash libconfig nodejs npm python-pip libsixel imagemagick --noconfirm || exit
+
+    if [ -e /etc/systemd/system/display-manager.service ]; then 
+        default_target=$(basename $(readlink -f /etc/systemd/system/display-manager.service))
+        login_manager=${default_target%-*}
+        sudo systemctl disable $login_manager || exit
+        yay -S sddm --noconfirm || exit
+        sudo systemctl enable sddm.service || exit
+    else
+      yay -S sddm --noconfirm || exit
+      sudo systemctl enable sddm.service || exit
+    fi
 
     git clone https://github.com/fdev31/picom || exit
     cd picom || exit
@@ -41,6 +52,11 @@ configure_dots() {
     echo "(5/8) Copying desktop configuration files..."
     cp desktop/xsession $HOME/.xsession
     sudo cp desktop/dwm.desktop /usr/share/xsessions || exit
+    sudo cp -r desktop/tartarus -r /usr/share/grub/themes || exit
+    sudo mv desktop/grub /etc/default/grub || exit
+    sudo grub-mkconfig -o /boot/grub/grub.cfg || exit
+    sudo cp -r desktop/sugar-candy /usr/share/sddm/themes || exit
+    sudo mv desktop/default.conf /usr/lib/sddm/sddm.conf.d/default.conf || exit
 
     echo "(6/8) Building dwm and dwmblocks..."
     sudo make clean install -C desktop/dwm || exit
@@ -51,10 +67,7 @@ configure_dots() {
 
     echo "(8/8) Configuring terminal..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    mkdir -p ~/.config/fzf/shell
-    wget -P ~/.config/fzf/shell/ https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh 
     rm $HOME/.zshrc
-    cp shell/fzf.zsh $HOME/.fzf.zsh || exit
     cp shell/zshrc $HOME/.zshrc || exit
     echo "DONE!"
 }
